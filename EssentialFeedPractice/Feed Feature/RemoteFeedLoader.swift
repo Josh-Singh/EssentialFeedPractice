0b5f9c0758eed43d9c6a95cf30288b682125c916
Josh Singh <josh.singh0006@gmail.com>
2023-04-06 15:58:40 -0400

    Inject URL into RemoteFeedLoader
    Removes hard coding of URL in the RemoteFeedLoader

--- EssentialFeedPractice/Feed Feature/RemoteFeedLoader.swift
+++ EssentialFeedPractice/Feed Feature/RemoteFeedLoader.swift
@@ -8,13 +8,15 @@
 import Foundation
 
 class RemoteFeedLoader {
-    let client:HTTPClient
+    let client: HTTPClient
+    let url: URL
     
-    init(client: HTTPClient) {
+    init(client: HTTPClient, url: URL) {
         self.client = client
+        self.url = url
     }
     
     func load() {
-        client.get(from: URL(string: "https://some-url.com")!)
+        client.get(from: url)
     }
 }
--- EssentialFeedPracticeTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedPracticeTests/RemoteFeedLoaderTests.swift
@@ -10,20 +10,23 @@ import XCTest
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_initDoesNotRequestURL() {
+        let url = URL(string: "https://some-url.com")!
         let client = HTTPClientMock()
-        _ = RemoteFeedLoader(client: client)
+        _ = RemoteFeedLoader(client: client, url: url)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_loaddataFromRequestURL() {
         // Testing the load feed command from API
+        let url = URL(string: "https://some-url.com")!
         let client = HTTPClientMock()
-        let sut = RemoteFeedLoader(client: client)
+        let sut = RemoteFeedLoader(client: client, url: url)
         
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(client.requestedURL, url)
     }
 
 }
