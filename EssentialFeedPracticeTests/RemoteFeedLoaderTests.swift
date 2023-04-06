ee37a28f77b838ca570926b5304918ea4e211e14
Josh Singh <josh.singh0006@gmail.com>
2023-04-06 16:16:47 -0400

    Removing makeSUT method to get rid of weird compiler errors

--- EssentialFeedPracticeTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedPracticeTests/RemoteFeedLoaderTests.swift
@@ -10,7 +10,9 @@ import XCTest
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_initDoesNotRequestURL() {
-        let (_, client) = makeSUT()
+        let url = URL(string: "https://some-new-url.com")!
+        let client = HTTPClientMock()
+        let _  = RemoteFeedLoader(client: client, url: url)
         
         XCTAssertNil(client.requestedURL)
     }
@@ -18,18 +20,13 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_loaddataFromRequestURL() {
         // Testing the load feed command from API
         let url = URL(string: "https://some-new-url.com")!
-        let (sut, client) = makeSUT(url: url)
+        let client = HTTPClientMock()
+        let sut = RemoteFeedLoader(client: client, url: url)
         
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
         XCTAssertEqual(client.requestedURL, url)
-    }
-    
-    // MARK: Helpers
-    private func makeSUT(client: HTTPClientMock = HTTPClientMock(),
-                         url: URL = URL(string: "https://some-url.com")!) -> (RemoteFeedLoader, HTTPClient) {
-        return (RemoteFeedLoader(client: client, url: url), client)
     }
 
 }
