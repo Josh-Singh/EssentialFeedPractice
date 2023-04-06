8d696a1e8de8ddd230436e7242a919087d327d21
Josh Singh <josh.singh0006@gmail.com>
2023-04-06 16:12:47 -0400

    Adding helper makeSUT method
    Compiler errors even though there shouldn't be any...

--- EssentialFeedPracticeTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedPracticeTests/RemoteFeedLoaderTests.swift
@@ -10,23 +10,26 @@ import XCTest
 class RemoteFeedLoaderTests: XCTestCase {
 
     func test_initDoesNotRequestURL() {
-        let url = URL(string: "https://some-url.com")!
-        let client = HTTPClientMock()
-        _ = RemoteFeedLoader(client: client, url: url)
+        let (_, client) = makeSUT()
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_loaddataFromRequestURL() {
         // Testing the load feed command from API
-        let url = URL(string: "https://some-url.com")!
-        let client = HTTPClientMock()
-        let sut = RemoteFeedLoader(client: client, url: url)
+        let url = URL(string: "https://some-new-url.com")!
+        let (sut, client) = makeSUT(url: url)
         
         sut.load()
         
         XCTAssertNotNil(client.requestedURL)
         XCTAssertEqual(client.requestedURL, url)
+    }
+    
+    // MARK: Helpers
+    private func makeSUT(client: HTTPClientMock = HTTPClientMock(),
+                         url: URL = URL(string: "https://some-url.com")!) -> (RemoteFeedLoader, HTTPClient) {
+        return (RemoteFeedLoader(client: client, url: url), client)
     }
 
 }
